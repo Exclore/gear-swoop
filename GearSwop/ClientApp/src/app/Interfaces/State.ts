cb65a2f4453a28c7ef7d4928c1d9fec33ef78ef3
Shawn Gallagher <Gshawn017@gmail.com>
2020-09-11 02:04:56 -0600

    Test data for the front-end state.

--- GearSwop/ClientApp/src/app/Interfaces/State.ts
+++ GearSwop/ClientApp/src/app/Interfaces/State.ts
@@ -1,5 +1,37 @@
-import { ISwap } from './Swap';
-
-export interface IState {
-  swaps: Array<ISwap>
+export var IState: { [characterName: string]: { [job: string]: { [setName: string] : any } } } = {
+'Exclore': {
+    'WAR': {
+      'SavageBlade': {
+        Mode: 'Precast',
+        Main: 21621,
+        Sub: 27644
+      },
+      'SmashySmash': {
+        Mode: 'Precast',
+        Main: 21621,
+        Feet: 27494
+      },
+    },
+    'MNK': [],
+    'WHM': [],
+    'BLM': [],
+    'RDM': [],
+    'THF': [],
+    'PLD': [],
+    'DRK': [],
+    'BST': [],
+    'BRD': [],
+    'RNG': [],
+    'SAM': [],
+    'NIN': [],
+    'DRG': [],
+    'SMN': [],
+    'BLU': [],
+    'COR': [],
+    'PUP': [],
+    'DNC': [],
+    'SCH': [],
+    'GEO': [],
+    'RUN': []
+  },
 }
